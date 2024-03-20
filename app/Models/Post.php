059fe8b8982256a9a79cc2f01187755bbc81328b
lolkiraly2 <szebigeri@gmail.com>
2024-03-20 16:09:12 +0100

    5-6. óra

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use HasFactory, SoftDeletes;
+    //protected $fillable = ['slug', 'body', 'published_at'];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     public static function getLength($post){
         return strlen($post);
     }
